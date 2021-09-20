761b6331f28606f52e35bcd27a8405a394ae4711
Sergey N <sergey.norov@yandex.ru>
2021-09-20 09:44:35 +0300

    Show divisions

--- src/apps/hrlist/views.py
+++ src/apps/hrlist/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+
+from src.apps.hrlist.models import Division, Staff
 
 
-def home(request):
-    return render(request, 'base.html')
+class HomeView(TemplateView):
+    # model = Division
+    paginate_by = 6
+    template_name = "home.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['divisions'] = Division.objects.all()
+        return context
